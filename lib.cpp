e50a08138918fcaffc63a17e77b1e71bb398e740
JacopoRampa <91597718+JacopoRampa@users.noreply.github.com>
2021-11-28 17:48:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include "lib.h"
+bool lettera_altro(char carattere){
+    if ((carattere>=65 && carattere<=90) || (carattere>=97 && carattere<=122)){
+        return true;
+    }else{
+        return false;
+    }
+}
+char minuscolo_maiuscolo(char carattere){
+    if (carattere>=65 && carattere<=90){
+        carattere = carattere + 32;
+        return carattere;
+    }else if (carattere>=97 && carattere<=122){
+        carattere = carattere - 32;
+        return carattere;
+    }
+}
