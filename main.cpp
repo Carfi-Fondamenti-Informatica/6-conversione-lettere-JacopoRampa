9188caf79267b5f5136d99744be5762a835e50a8
JacopoRampa <91597718+JacopoRampa@users.noreply.github.com>
2021-11-28 17:49:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    char carattere='0';
+    cin >> carattere;
+    if(lettera_altro(carattere)){
+        cout << minuscolo_maiuscolo(carattere) << endl;
+    }
+    else{
+        cout << "errore";
+    }
+    return 0;
 }
